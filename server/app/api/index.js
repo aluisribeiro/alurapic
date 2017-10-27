d0161c44cb00e8340e5d12140c85b5589b5cc9c0
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2017-10-27 13:01:03 -0200

    limpando logs desnecessarios

--- server/app/api/index.js
+++ server/app/api/index.js
@@ -15,7 +15,6 @@ api.adiciona = function(req, res) {
 };
 
 api.busca = function(req, res) {
-    console.log(req.params.fotoId);
     connection().query("select * from fotos where _id = ?",[req.params.fotoId], function(err, doc) {
         if (err) return console.log(err);
         res.json(doc[0]);
